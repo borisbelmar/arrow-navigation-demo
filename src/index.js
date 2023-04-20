353b8e145cfbb758cd5884b85e68e0356ee092d2
Boris Belmar <borisbelmarm@gmail.com>
2023-04-19 23:47:50 -0400

    Update README.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './styles.css'
-import { initArrowNavigation } from '@borisbelmar/arrow-navigation'
+import { initArrowNavigation } from '@arrow-navigation/core'
 
 const arrowNavigationApi = initArrowNavigation()
+
+const getCenterPoint = element => {
+  const rect = element.getBoundingClientRect()
+  return {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2
+  }
+}
+
+const getInnerTextByPoint = (element, label) => {
+  const centerPoint = getCenterPoint(element)
+  return `${label} X:${Math.round(centerPoint.x)} Y:${Math.round(centerPoint.y)}`
+}
 
 const body = document.querySelector('body')
 body.classList.add('bg-gray-800', 'h-screen', 'flex', 'flex-col', 'justify-center', 'items-center')
@@ -28,28 +41,28 @@ const createButton = text => {
 const createGrid = (...classList) => {
   const gridContainer = document.createElement('div')
   gridContainer.classList.add('grid', 'grid-cols-3', 'gap-4', 'w-1/2', ...classList)
-
+  body.appendChild(gridContainer)
   Array.from({ length: 9 }, (_, i) => {
     const button = createButton(i + 1)
     gridContainer.appendChild(button)
+    button.innerText = getInnerTextByPoint(button, i + 1)
     return button
   })
-
-  return gridContainer
 }
 
-Array.from({ length: 3 }, _ => {
-  const grid = createGrid('mb-8')
-  body.appendChild(grid)
+Array.from({ length: 3 }, (_, idx) => {
+  createGrid('mb-8', 'relative', idx % 2 === 0 ? 'left-10' : undefined)
 })
 
 const absoluteButton = createButton('Absolute')
 absoluteButton.classList.add('absolute', 'bottom-4', 'right-4', 'm-8', 'bg-orange-500', 'focus:ring-green-500')
 
 body.appendChild(absoluteButton)
+absoluteButton.innerText = getInnerTextByPoint(absoluteButton, 'Absolute')
 
 const absoluteButton2 = createButton('Absolute')
 absoluteButton2.classList.add('absolute', 'top-4', 'left-2', 'm-8', 'bg-purple-500', 'focus:ring-red-500')
 
 body.appendChild(absoluteButton2)
+absoluteButton2.innerText = getInnerTextByPoint(absoluteButton2, 'Absolute')
 
